c703464e2029f2123560409ec884eee32ecef3a3
Daniel Tuukkanen <tuukkanen.daniel@gmail.com>
2026-01-17 15:43:22 +0200

    Add Header component to ComponentsPage

--- app/components/page.tsx
+++ app/components/page.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import HelloWorld from "./HelloWorld";
 import Button from "./Button";
+import Header from "./Header";
 
 const ComponentsPage = () => {
     return (
-        <div>
+        <div className="bg-primary">
             <h2 className="text-accent text-center">Components Page</h2>
             <div className="flex flex-col items-center gap-4 m-4">
+                <Header />
                 <HelloWorld />
                 <Button>
                     Test clicking!
