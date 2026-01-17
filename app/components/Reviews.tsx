32dbcb32385e7d9ad88e07c1e355d259000aeb22
Daniel Tuukkanen <tuukkanen.daniel@gmail.com>
2026-01-17 16:38:43 +0200

    Add Reviews component to display movie ratings

---
+++ app/components/Reviews.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import Image from 'next/image';
+
+export interface ReviewsProps {
+    IMDBRating?: string;
+    RottenTomatoesRating?: string;
+    MetacriticRating?: string;
+}
+
+interface RatingItemProps {
+    src: string;
+    alt: string;
+    rating: string;
+}
+
+const RatingItem: React.FC<RatingItemProps> = ({ src, alt, rating }) => (
+    <div className="flex flex-row items-center gap-1">
+        <Image src={src} alt={alt} width={24} height={24} />
+        <p>{rating}</p>
+    </div>
+);
+
+const Reviews: React.FC<ReviewsProps> = ({ IMDBRating, RottenTomatoesRating, MetacriticRating }) => {
+    return (
+        <div className='flex flex-row text-2xl font-black text-text justify-between w-full max-w-md'>
+            {IMDBRating && <RatingItem src="/IMDb_logo.svg" alt="IMDB" rating={IMDBRating} />}
+            {RottenTomatoesRating && <RatingItem src="/Rotten_Tomatoes_logo.svg" alt="Rotten Tomatoes" rating={RottenTomatoesRating} />}
+            {MetacriticRating && <RatingItem src="/Metacritic_logo.svg" alt="Metacritic" rating={MetacriticRating} />}
+        </div>
+    );
+};
+
+export default Reviews;
