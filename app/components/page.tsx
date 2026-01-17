d381b58201c8ea1ee60f00387df89fa1d986004f
Daniel Tuukkanen <tuukkanen.daniel@gmail.com>
2026-01-17 15:21:25 +0200

    Add margin to components layout and include Button component

--- app/components/page.tsx
+++ app/components/page.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import HelloWorld from "./HelloWorld";
+import Button from "./Button";
 
 const ComponentsPage = () => {
     return (
         <div>
             <h2 className="text-accent text-center">Components Page</h2>
-            <div className="flex flex-col items-center gap-4">
+            <div className="flex flex-col items-center gap-4 m-4">
                 <HelloWorld />
+                <Button>
+                    Test clicking!
+                </Button>
             </div>
         </div>
     );
