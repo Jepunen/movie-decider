c096e174e6df7f19b1ffccfa98e324b2951b98d4
Daniel Tuukkanen <tuukkanen.daniel@gmail.com>
2026-01-23 10:28:58 +0200

    Move sample data to better place while building components

--- app/page.tsx
+++ app/page.tsx
@@ -3,33 +3,6 @@
 import Image from "next/image";
 import { useState, useEffect } from "react";
 import { socket } from "./socket";
-import { disconnect } from "process";
-import MovieForRating from "./components/MovieForRating";
-import Header from "./components/Header";
-import { Movie } from "@/types/movies";
-
-// SAMPLE MOVIE DATA for testing MovieForRating component - R.M.
-const sampleMovie: Movie = {
-  adult: false,
-  backdrop_path: null,
-  genre_ids: [12, 878, 18],
-  id: 101,
-  original_language: "en",
-  original_title: "Avatar: Fire and Ash",
-  overview: "In the wake of the devastating war against the RDA and the loss of their eldest son, Jake Sully and Neytiri face a new threat on Pandora: the Ash People, a violent and power-hungry Na'vi tribe led by the ruthless Varang. Jake's family must fight for their survival and the future of Pandora in a conflict that pushes them to their emotional and physical limits.",
-  popularity: 10.5,
-  poster_path: "https://image.tmdb.org/t/p/w500/bRBeSHfGHwkEpImlhxPmOcUsaeg.jpg",
-  release_date: "2025-12-17",
-  title: "Avatar: Fire and Ash",
-  video: false,
-  vote_average: 8.0,
-  vote_count: 10000,
-};
-
-// SAMPLE RATINGS for testing MovieForRating component - R.M.
-const sampleIMDBRating = "8.5/10";
-const sampleRottenTomatoesRating = "92%";
-const sampleMetacriticRating = "85/100";
 
 export default function Home() {
   const [isConnected, setIsConnected] = useState<boolean>(false);
