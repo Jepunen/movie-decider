38c0a81142ff946a9ef33feb31470e9b583b472e
roopemyller <roope.myller@gmail.com>
2026-01-26 20:57:14 +0200

    created the VotingPage structure, made fixes and improvements to the MovieCard so it fits better on the VotingPage, added mock list of movies to test voting on, added framer-motion for animations

--- app/components/MovieCard/MovieCardBack.tsx
+++ app/components/MovieCard/MovieCardBack.tsx
@@ -9,7 +9,7 @@ interface MovieCardBackProps {
 export const MovieCardBack: React.FC<MovieCardBackProps> = ({ movie, onClick }) => {
   return (
     <div
-      className="p-4 rounded shadow max-w-md bg-secondary front h-150 flex flex-col overflow-y-auto"
+      className="p-4 rounded-xl shadow w-sm bg-secondary front h-120 flex flex-col overflow-y-auto"
       onClick={onClick}
     >
       <h3 className="text-2xl font-bold mb-2">{movie.title ?? "Unknown Title"}</h3>
--- app/components/MovieCard/MoviePoster.tsx
+++ app/components/MovieCard/MoviePoster.tsx
@@ -8,12 +8,13 @@ interface MoviePosterProps {
 
 export const MoviePoster: React.FC<MoviePosterProps> = ({ posterPath, title }) => {
   return (
-    <div className="relative h-96 w-full mb-4 shrink-0">
+    <div className="relative h-76 w-48 mb-2 shrink-0 self-center">
       <Image
+        loading="lazy"
         src={posterPath ?? "/movie_posters_DEV/the_martian.jpg"}
         alt={title ?? "Movie Poster"}
         fill
-        className="rounded object-contain"
+        className="object-cover rounded-lg shadow-md"
         sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
       />
     </div>
