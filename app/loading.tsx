4723c52cdf189260785b70115671a1a590087a3b
roopemyller <roope.myller@gmail.com>
2026-03-03 15:58:22 +0200

    fixed issue with loading screen being scrollable and fixed result card modal background color

--- app/loading.tsx
+++ app/loading.tsx
@@ -2,7 +2,7 @@ import Loading from "./components/Loading";
 
 export default function GlobalLoading() {
     return (
-        <div className="flex items-center justify-center min-h-screen">
+        <div className="fixed inset-0 z-50 flex items-center justify-center overflow-hidden">
             <Loading />
         </div>
     );
